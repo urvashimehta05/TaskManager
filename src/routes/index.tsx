82ffd6277b4441f46b9c49611cc318197e505cfa
urvashimehta05 <urvashimehta552@gmail.com>
2026-03-20 17:20:17 +0530

    validate UrL

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router"
 import { TaskTable } from "#/tasks/components/task-table"
-
+import { z } from 'zod'
+const productSearchSchema = z.object({
+  search: z.string().catch(""),
+  status: z.enum(["all", "todo", "in-progress", "done"]).catch("all"),
+  priority: z.enum(["all", "low", "medium", "high"]).catch("all"),
+})
 export const Route = createFileRoute("/")({
   component: TasksPage,
 
-  validateSearch: (search) => ({
-    search: search.search ?? "",
-    status: search.status ?? "all",
-    priority: search.priority ?? "all",
-  }),
+ validateSearch: productSearchSchema,
 })
 
 function TasksPage() {
